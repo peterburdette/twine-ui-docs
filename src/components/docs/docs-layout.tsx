5486878458a5ff49097c9067166da31cb0757a5d
Peter Burdette <peterburdette@gmail.com>
2025-08-18 15:11:59 -0400

    responsive design fix

--- src/components/docs/docs-layout.tsx
+++ src/components/docs/docs-layout.tsx
@@ -1,15 +1,15 @@
-"use client"
+'use client';
 
-import type * as React from "react"
-import { DocsProvider } from "@/components/docs/docs-provider"
-import { DocsSidebar } from "@/components/docs/docs-sidebar"
-import { TableOfContents } from "@/components/docs/table-of-contents"
-import { Footer } from "@/components/docs/footer"
-import { Header } from "@/components/docs/header"
+import type * as React from 'react';
+import { DocsProvider } from '@/components/docs/docs-provider';
+import { DocsSidebar } from '@/components/docs/docs-sidebar';
+import { TableOfContents } from '@/components/docs/table-of-contents';
+import { Footer } from '@/components/docs/footer';
+import { Header } from '@/components/docs/header';
 
 interface DocsLayoutProps {
-  children: React.ReactNode
-  currentPage?: string
+  children: React.ReactNode;
+  currentPage?: string;
 }
 
 export function DocsLayout({ children, currentPage }: DocsLayoutProps) {
@@ -23,9 +23,9 @@ export function DocsLayout({ children, currentPage }: DocsLayoutProps) {
         <DocsSidebar />
 
         {/* Content area - takes remaining space after sidebar */}
-        <div className="flex-1 lg:ml-64 flex">
+        <div className="flex-1 lg:ml-64 flex min-w-0">
           {/* Main content - centered in available space */}
-          <div className="flex-1 xl:max-w-[calc(100vw-32rem)] flex justify-center px-4 sm:px-6 lg:px-8">
+          <div className="flex-1 flex justify-center px-4 sm:px-6 lg:px-8 min-w-0">
             <main className="w-full max-w-4xl min-w-0">
               {children}
               <Footer />
@@ -37,5 +37,5 @@ export function DocsLayout({ children, currentPage }: DocsLayoutProps) {
         </div>
       </div>
     </DocsProvider>
-  )
+  );
 }
